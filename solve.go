48b726b0db1debf63e93cb98739c95ffb29dd505
Max Timkovich <mtimkovich@gmail.com>
2018-10-11 16:29:47 -0700

    error case where there are fewer words than attempts

--- solve.go
+++ solve.go
@@ -53,7 +53,7 @@ func Solve(tiles Tiles) *Solution {
 	const ATTEMPTS = 50
 
 	words := highestWords(tiles, ATTEMPTS)
-	queue := make(chan *Solution, len(words))
+	queue := make(chan *Solution, words.Len())
 
 	for _, word := range words {
 		go func(word *WordAndScore) {
@@ -67,7 +67,7 @@ func Solve(tiles Tiles) *Solution {
 		}(word)
 	}
 
-	for i := 0; i < len(words); i++ {
+	for i := 0; i < words.Len(); i++ {
 		newSolution := <-queue
 		if newSolution != nil && newSolution.Total() > solution.Total() {
 			solution = newSolution
